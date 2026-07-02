85c4b79f57f0c10c692e20a35e3809cb9f869a23
farhan1515 <farhananwar784@gmail.com>
2026-07-02 10:00:00 -0400

    Add urls routes for root project

--- threadline/urls.py
+++ threadline/urls.py
@@ -1,22 +1,29 @@
-"""
-URL configuration for threadline project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Login/logout/password-reset use Django's built-in auth views rather than
+# hand-rolled ones - the reset flow (request -> email -> reset link -> new
+# password) is annoying to get right and Django already does it.
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from store.forms import LoginForm, PasswordResetRequestForm, PasswordResetConfirmForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # login / logout (templates live in templates/registration/)
+    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # ---- forgot password (4-step built-in flow) ----
+    path('password_reset/', auth_views.PasswordResetView.as_view(form_class=PasswordResetRequestForm), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(form_class=PasswordResetConfirmForm), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    path('', include('store.urls')),
 ]
+
+# Serve uploaded media in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
